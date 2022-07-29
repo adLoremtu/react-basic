b20eb5f372831b39e70788813e49acfaf496cf28
takuya inoue <takuya.inoue@nijibox.co.jp>
2022-07-29 22:02:33 +0900

    No4の修正

---
+++ react-basic-answer/src/components/answer/No4/index.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import SampleLink from "../../ui/link";
+
+/**
+ * components/ui/link/index.tsxを編集し、要素を表示してください。
+ *
+ * 【ヒント】
+ * 実装の流れとしては下記順番で組むとわかりやすいです。
+ *
+ * 1. components/ui/link/index.tsxを編集
+ * 2. コンポーネントファイルを呼び出す
+ * 3. 呼び出したコンポーネントを表示させる
+ */
+
+const No4 = () => {
+  return (
+    <>
+      <h2>問題4:</h2>
+      {/* === 問題4の解答  start  === */}
+      <SampleLink />
+      {/* === 問題4の解答  end  === */}
+    </>
+  );
+};
+
+export default No4;
