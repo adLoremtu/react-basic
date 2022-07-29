ab5f02dc969fd730399f78fd79e731a8dfec5802
takuya inoue <takuya.inoue@nijibox.co.jp>
2022-07-29 20:40:53 +0900

    No.3修正

--- react-basic-answer/src/answer/No3.tsx
+++ react-basic-answer/src/answer/No3.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import SampleButton from "../components/sample/Button";
 
+/**
+ * 問題3:
+ *   components/sample/Button.tsxを呼び出し、要素を表示してください。
+ *
+ * 【ヒント】
+ * コンポーネントファイルを使う際、2つの手順を踏む必要があります。
+ * 1. コンポーネントファイルを呼び出す
+ * 2. 呼び出したコンポーネントを表示させる
+ */
+
 const No3 = () => {
   return (
     <>
-      <h2>
-        問題3: components/sample/Button.tsxを呼び出し、要素を表示してください。
-      </h2>
+      <h2>問題3:</h2>
       {/* === 問題3の解答  start  === */}
       <SampleButton />
       {/* === 問題3の解答  end  === */}
