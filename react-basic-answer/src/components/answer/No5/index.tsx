d16a4a8432411d858e4ab2c95756a58ea6b2b271
takuya inoue <takuya.inoue@nijibox.co.jp>
2022-07-31 21:32:50 +0900

    No5の修正完了

---
+++ react-basic-answer/src/components/answer/No5/index.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import PropsTest from "../../ui/list/";
+
+/**
+ * 問題5:
+ * components/ui/list/index.tsxを呼び出し、要素を表示してください。・
+ *
+ * またその際、コンポーネントファイルに下記パラメータを送付するようにしてください。
+ * - name: 自分の名前 or あだ名(string)
+ * - age: 自分の年齢(number)
+ * - isMen: 性別(boolean)
+ *   - 男性の場合はtrue
+ *   - 女性の場合はfalse
+ */
+
+const No5 = () => {
+  const name = "ボン";
+  const age = 28;
+  const isMen = true;
+
+  return (
+    <>
+      <h2>問題5:</h2>
+      {/* === 問題5の解答  start  === */}
+      <PropsTest name={name} age={age} isMen={isMen} />
+      {/* === 問題5の解答  end  === */}
+    </>
+  );
+};
+
+export default No5;
---
+++ react-basic/src/components/answer/No5/index.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+/**
+ * 問題5:
+ * components/ui/list/index.tsxを呼び出し、要素を表示してください。・
+ *
+ * またその際、コンポーネントファイルに下記パラメータを送付するようにしてください。
+ * - name: 自分の名前 or あだ名(string)
+ * - age: 自分の年齢(number)
+ * - isMen: 性別(boolean)
+ *   - 男性の場合はtrue
+ *   - 女性の場合はfalse
+ */
+
+const No5 = () => {
+  return (
+    <>
+      <h2>問題5:</h2>
+      {/* === 問題5の解答エリア  start  === */}
+      {/* === 問題5の解答エリア  end  === */}
+    </>
+  );
+};
+
+export default No5;
